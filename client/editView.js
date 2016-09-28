8854df65cb62afdc9b18ed860e8ac27672be4e50
Jan Manuel Schlieper <manuel.schlieper@beyondrm.com>
2016-09-28 20:46:22 +0200

    added update on keyup added validation on keyup fixed structure

---
+++ client/editView.js
@@ -0,0 +1,62 @@
+import {RESTCollection} from 'meteor/dasdeck:restcollection';
+
+FlowRouter.route('/edit/:collection/:id/:type?', {
+    action: function (params) {
+        if (params.type === 'oid') {
+            params.id = new Mongo.ObjectID(params.id);
+        }
+        BlazeLayout.render("layout", {content: "editView", params: params});
+    }
+});
+
+Template.editView.helpers({
+    data: function () {
+        var entity = this.collection.findOne({_id: this.id});
+        return entity
+    },
+    schema: function () {
+        return this.collection.getSchema('edit');
+    },
+    class:function(){
+
+    }
+});
+
+
+Template.editView.events({
+
+    'keyup, change': function (e) {
+        e.preventDefault();
+        let data = Template.instance().data;
+        let collection = data.collection;
+        let formName = collection._name;
+        if (AutoForm.validateForm(formName)) {
+            let entity = collection.findOne({_id: data.id});
+            if (e.target.type === 'number') {
+                entity[e.target.name] = e.target.valueAsNumber;
+            }
+            else if (e.target.type === 'date') {
+                entity[e.target.name] = e.target.valueAsDate;
+            }
+            else {
+                entity[e.target.name] = e.target.value;
+
+            }
+            entity.saveDebounced();
+        }
+    }
+
+});
+
+
+Template.editView.onCreated(function () {
+
+    var params = this.data.params();
+    var collection = window[params.collection];
+    this.data.id = params.id;
+    this.data.collection = collection;
+
+    if (!(collection instanceof RESTCollection)) {
+        Meteor.subscribe(collection._name, {filter: {_id: params.id}});
+    }
+});
---
+++ package.js
@@ -0,0 +1,38 @@
+Package.describe({
+    name: 'dasdeck:data-edit',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'quickly edit collection entities',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.3');
+    api.use('templating', 'client');
+    api.use('blaze-html-templates');
+    api.use('less');
+    api.use('msgfmt:core');
+    api.use('ecmascript');
+    api.use('mongo');
+    api.use('aldeed:autoform');
+    api.use('aldeed:collection2');
+    api.use('kadira:flow-router');
+    api.use('dasdeck:restcollection');
+
+
+    api.addFiles('client/editView.html', 'client');
+    api.addFiles('client/style.less', 'client');
+
+    api.addFiles('client/editView.js', 'client');
+});
+
+Package.onTest(function (api) {
+    //api.use('ecmascript');
+    //api.use('tinytest');
+    //api.use('mongo-object');
+    //api.addFiles('mongo-object-tests.js');
+});
